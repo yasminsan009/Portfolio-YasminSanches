d8f300b21d598ebec917508d3762fc5b1cb7d476
Yasmin <yasminsan19009@gmail.com>
2025-01-17 19:25:05 -0300

    commit

--- assets/JS/api.js
+++ assets/JS/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/yasminsan009/Portf-lio/refs/heads/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/yasminsan009/Portf-lio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
--- assets/JS/main.js
+++ assets/JS/main.js
@@ -9,24 +9,24 @@ function updateProfileInfo(profileData) {
 
 function updateHardSkills(profileData) {
     const hardSkills = document.getElementById('profile.skills.hardSkills')
-    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => 
+        `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`
+    ).join('');
 }
 
 function updateSoftSkills(profileData) {
-    const softSkills = document.getElementById('profile.skills.softSkills')
-    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
-}
-
-function updateLanguages(profileData) {
-    const languages = document.getElementById('profile.languages')
-    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
+    const softSkills = document.getElementById('profile.skills.softSkills');
+    console.log("Soft Skills Data:", profileData.skills.softSkills); // Verifique os dados
+    softSkills.innerHTML = profileData.skills.softSkills
+        .map(skill => `<li>${skill}</li>`)
+        .join('');
 }
 
 function updatePortfolio(profileData) {
     const portfolio = document.getElementById('profile.portfolio');
     portfolio.innerHTML = profileData.portfolio.map(project => {
         return `
-            <li>
+            <li id="neon">
                 <a href="${project.url}" target="_blank" ${project.github ? 'class="github"' : ''}>
                     ${project.name}
                 </a>
@@ -34,11 +34,12 @@ function updatePortfolio(profileData) {
         `;
     }).join('');
 }
-(async () => {
-    const profileData = await fetchProfileData()
-    updateProfileInfo(profileData)
-    updateHardSkills(profileData)
-    updateSoftSkills(profileData)
-    updateLanguages(profileData)
-    updatePortfolio(profileData)
-})()
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const profileData = await fetchProfileData();
+    console.log("Profile Data:", profileData); // Ver
+    updateProfileInfo(profileData);
+    updateHardSkills(profileData);
+    updateSoftSkills(profileData);
+    updatePortfolio(profileData);
+});
